d0c88329eee78fc581d6e87185c2cee4adbb2275
Sizzizzle <sasasas@gmail.com>
2010-12-22 16:51:56 +0000

    Update to contact form structure

--- forms.py
+++ forms.py
@@ -1,16 +1,23 @@
 from django import forms
 
+ORG_TYPE_CHOICES = (
+	('individual', 'No'),
+	('church', 'Yes (my church)'),
+	('other', 'Yes (other organisation)'),
+)
+
 NEWSLETTER_CHOICES = (
 	('Y', 'Yes'),
 	('N', 'No'),
 )
 
 class ContactForm(forms.Form):
+	org_type = forms.ChoiceField(choices=ORG_TYPE_CHOICES, widget=forms.RadioSelect, label='Are you contacting EoSGP on behalf of your church or an organisation?')	
 	name = forms.CharField(max_length=50)
-	name_of_minister = forms.CharField(max_length=50)
-	church = forms.CharField(max_length=100)
+	church_or_org = forms.CharField(max_length=100, label='Church or organisation (if applicable)', required=False)
+	minister_or_head = forms.CharField(max_length=50, label='Minister/head of organisation (if applicable)', required=False)
 	email = forms.EmailField()
-	address = forms.CharField(max_length=200)
+	address = forms.CharField(max_length=200, widget=forms.Textarea)
 	message = forms.CharField(required=False, widget=forms.Textarea)
-	newsletter = forms.ChoiceField(choices=NEWSLETTER_CHOICES, widget=forms.RadioSelect)
+	newsletter = forms.ChoiceField(choices=NEWSLETTER_CHOICES, widget=forms.RadioSelect, label='Would you like to receive our newsletter to the address you have given us?')
 
