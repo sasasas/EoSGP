81ad188b0b70b9537baab6224be1a79f4bdf998c
Chris Causer <chy.causer@gmail.com>
2011-01-02 11:58:03 +0000

    Improved static pages, but broken due to incorrect fixtures

--- content/models.py
+++ content/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+class FlatPageManager(models.Manager):
+	def get_page_or_blank(self, **kwargs):
+		try:
+			return self.get(**kwargs)
+		except ObjectDoesNotExist:
+			# Construct blank page
+			permalink = kwargs.get('permalink', None)
+			title = kwargs.get('title', permalink)
+			content = ''' This page does not exist.'''
+			return FlatPage(permalink = permalink, title = title, content=content)
 
 class FlatPage(models.Model):
 	permalink = models.CharField(max_length=100, unique=True)
+	template_file = models.CharField(max_length=100, default='flat_page.html')
 	title = models.CharField(max_length=100, blank=True)
+	objects = FlatPageManager()
 	subtitle = models.CharField(max_length=100, null=True, blank=True)
 	content = models.TextField()
 
