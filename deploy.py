c0756ff97b54984a499ff2a095237ccb481edfdc
Chris Causer <c.causer1@physics.ox.ac.uk>
2011-01-05 11:27:34 +0000

    Fix error in deploy.py

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 #Paths of the CWD and its parent are added to the PYTHONPATH
-paths = [os.path.join(os.path.dirname(__file__), os.path.join(os.path.dirname(os.path.dirname(__file__))]
+paths = [os.path.dirname(__file__), os.path.dirname(os.path.dirname(__file__))]
 for path in paths:
   if path not in sys.path:
     sys.path.append(path)
